8101fcfa5837240b0b82b51f8bef5ff956c46a9d
Mpatrick12 <p.mukunzi@alustudent.com>
2023-11-03 02:11:52 -0700

    m

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, math
+    import sys
     result = 0
-    for i in sys.argv:
-        result += int(i)
+    for arg in sys.argv[1:]:
+        result += int(arg)
         print("{}".format(result))
