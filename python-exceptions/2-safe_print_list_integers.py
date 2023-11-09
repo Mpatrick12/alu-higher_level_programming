9d49320b38c0ac5197a1709e51c8376ae81628db
Mpatrick12 <p.mukunzi@alustudent.com>
2023-11-09 03:10:26 -0800

    m

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+def safe_print_list_integers(my_list=[], x=0):
+    total = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            total += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return total
